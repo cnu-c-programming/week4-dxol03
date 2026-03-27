68d91e2a8c5cd10711f9f02b281075066155b852
dxol03 <151935037+dxol03@users.noreply.github.com>
2026-03-27 14:02:19 +0900

    Update 1-3-2.c

--- code/1-3/1-3-2.c
+++ code/1-3/1-3-2.c
@@ -2,16 +2,16 @@
 
 #include "util.h"
 
-void logger_global(char* message) {
+void logger_gloval(char* message) {
     static int counter = 0;
     printf("log %2d: %s\n",counter++, message);
 }
 
 void task2() {
-    for (int i = 0; i < i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
         logger_static_inline("task2");
-        logger_global("task2");
+        logger_gloval("task2");
     }
     
 }
